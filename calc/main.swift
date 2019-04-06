d00b0914c851e69dc21ab8aa4ceacc1ca5feae0e
ike <survolt214@gmail.com>
2019-04-06 22:44:41 +1100

    calculater 1.0

--- calc/main.swift
+++ calc/main.swift
@@ -7,39 +7,42 @@
 //
 
 import Foundation
-/*
- // Instead of using hardcoded "+", how can you represent it with an enum?
- Enum Operator {
-    Addition
- }
- 
- Operator.Addition
- */
-var args = ProcessInfo.processInfo.arguments
-args.removeFirst() // remove the name of the program
-//print(Int(args[0])!)
 
-var split = args.split(separator: " ")
-print(split[0])
+var args = ProcessInfo.processInfo.arguments
+args.removeFirst()
+var split = args.split(separator: " ")              //input
 
 var times: Int = args.count
-var opsLevel = [Int](repeating: 0, count: times)
-var newNum = [Int](repeating: 0, count: times)
-var ops = [String]()
+var result = 0                                      //reset result
+var newNum = [Int](repeating: 0, count: times)      //number array
+var ops = [String](repeating: "", count: times)     //ops array
 
-var i = 0
-var j = 0
+var i = 0                                           //loop count
+var j = 0                                           //count of ops
+var k = 0                                           //count of numbers
 for i in 0...(times-1) {
-    if (args[i] == "*") || (args[i] == "/") || (args[i] == "%") {
-        ops[j] = args[i]
+    if (args[i] == "+") || (args[i] == "-") || (args[i] == "*") || (args[i] == "/") || (args[i] == "%") || (args[i] == "x") {
+        ops[j] = args[i]                            //ops array
+        j = j + 1
     }
-    if (args[i] != "+") && (args[i] != "-") && (args[i] != "*") && (args[i] != "/") && (args[i] != "%") {
-        newNum[i] = Int(args[i])!
-        print(newNum[i])
+    else {                                          //number array
+        newNum[k] = Int(args[i])!
+        k = k + 1
     }
 }
 
-var result = 0
+
+func checkError() {
+    if (k - j) > 1 {                                //if there are two or more numbers than ops, it is error
+        print("too much number")
+        exit(5)
+    }
+    if (k - j) < 1 {                                //if there are equal or less numbers than ops, it is error
+        print("too much ops")
+        exit(5)
+    }
+    
+}
 func calculate(mark: String, numLeft: Int, numRight: Int) {
     switch (mark) {
     case "+":
@@ -50,7 +53,7 @@ func calculate(mark: String, numLeft: Int, numRight: Int) {
         result = numLeft * numRight
     case "/":
         if numRight == 0 {
-            print("x / 0 dose not make sense")
+            print("you can not devide 0")
             exit(2)
         }
         else{
@@ -58,87 +61,62 @@ func calculate(mark: String, numLeft: Int, numRight: Int) {
         }
     case "%":
         result = numLeft % numRight
+    case "x":
+        result = numLeft * numRight
     default:
-        print("input error marks")
+        print("input error")
         exit(3)
+    
     }
 }
+if (args.count == 1) {                                                                    //display when only one number
+    checkError()
+    result = Int(args[0])!
+    print(result)
+}
 
-if (args.count <= 3) {   //calculate if only 3 elements here.
-    calculate(mark: args[1], numLeft: newNum[0], numRight: newNum[2])
-   /* print(newNum[0])
-      print(args[1])
-    print(newNum[2])*/
+if (args.count == 2) {                                                                    //always error
+    checkError()
+}
+
+if (args.count == 3) {                                                                    //calculate if only 3 elements here.（1+2）
+    checkError()
+    calculate(mark: args[1], numLeft: newNum[0], numRight: newNum[1])
+    print(result)
+    
+}
+
+if (args.count > 3) {                                                                     //calculate if more than 3 elements here.
+    checkError()
+    for j in 0...(((times-1)/2)-1) {
+        if (ops[j] == "*") || (ops[j] == "/") || (ops[j] == "%") || (ops[j] == "x") {     //calculate "*""/""%" first
+            calculate(mark: ops[j], numLeft: newNum[j], numRight: newNum[j+1])
+            if (j == 0) {                                                                 //if the fisrt ops is "*","/" or "%"
+                newNum[0] = 0
+                newNum[1] = result
+                ops[j] = "+"
+            }
+            else {                                                                        //delete numLeft and used ops, make numRight = result
+            newNum[j] = 0                                                                 //(5 * 7) = (0 + result)
+            newNum[j+1] = result
+            ops[j] = ops[j-1]
+            }
+        }
+    }
+    for j in 0...(((times-1)/2)-1) {
+        calculate(mark: ops[j], numLeft: newNum[j], numRight: newNum[j+1])
+        if (j == 0) {                                                                     //if the first ops is "+""-"
+            newNum[0] = 0
+            newNum[1] = result
+            ops[j] = "+"
+        }
+        else {
+        newNum[j] = 0
+        newNum[j+1] = result
+        ops[j] = "ops[j-1]"
+        }
+    }
     print(result)
     exit(0)
 }
 
-
-
-
-/*
-var num1:Int = Int(split[0])!
-var mark1 = String(split[1])
-var num2:Int = Int(split[2])!
-
-var num3 = 0
-var mark2 = ""
-
-if (split.count > 4){
-    mark2 = String(split[3])
-    num3 = Int(split[4])!
-    calcLevel = 1
-}
-if mark2 == "*" {
-    calcLevel = 2
-}
-if mark2 == "/"{
-    calcLevel = 2
-}
- 
- 
- func calculate(mark: String, newNum1: Int, newNum2: Int) {
- switch (mark) {
- case "+":
- result = newNum1 + newNum2
- case "-":
- result = newNum1 - newNum2
- case "*":
- result = newNum1 * newNum2
- case "/":
- if newNum2 == 0 {
- print("x / 0 dose not make sense")
- exit(2)
- }
- else{
- result = newNum1 / newNum2
- }
- case "%":
- result = newNum1 % newNum2
- default:
- print("input error marks")
- exit(3)
- }
- }
- 
- switch (calcLevel){
- case 0:
- calculate(mark: mark1, newNum1: num1, newNum2: num2)
- print(result)
- 
- case 1:
- calculate(mark: mark1, newNum1: num1, newNum2: num2)
- calculate(mark: mark2, newNum1: result, newNum2: num3)
- print(result)
- 
- case 2:
- calculate(mark: mark2, newNum1: num2, newNum2: num3)
- calculate(mark: mark1, newNum1: num1, newNum2: result)
- print(result)
- default:
- exit(4)
- }
- 
- 
- exit(0)
-*/
